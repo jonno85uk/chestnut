ed777d0ff17bc1eb3df1207ce8515264c3adbfba
jon noble <jonnobleuk@gmail.com>
2019-11-26 22:24:20 +0000

    viewer: corrected position of drawn gizmos #87 The rotation handle is on top, not bottom. The mouse-over feature of the gizmos are acting as if the gizmos are still being drawn incorrectly.

--- app/project/effect.cpp
+++ app/project/effect.cpp
@@ -943,7 +943,7 @@ void Effect::gizmo_world_to_screen()
       QVector4D screen_pos = QVector4D(g->world_pos[j].x(), g->world_pos[j].y(), 0, 1.0) * (view_matrix * projection_matrix);
 
       const int adjusted_sx1 = qRound(((screen_pos.x() * 0.5F) + 0.5F) * parent_clip->sequence->width());
-      const int adjusted_sy1 = qRound((1.0F - ((screen_pos.y() * 0.5F) + 0.5F)) * parent_clip->sequence->height());
+      const int adjusted_sy1 = qRound(((screen_pos.y() * 0.5F) + 0.5F) * parent_clip->sequence->height());
 
       g->screen_pos[j] = QPoint(adjusted_sx1, adjusted_sy1);
     }
